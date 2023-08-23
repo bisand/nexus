8ff774acdd75564b5d9a652cd3edd324e751b683
André Syvertsen Biseth <andre@biseth.net>
2023-08-23 14:02:49 +0000

    Client operations

--- server/Program.cs
+++ server/Program.cs
@@ -6,7 +6,9 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-class Program
+namespace nexus;
+
+public static class Program
 {
     static async Task Main(string[] args)
     {
